ff136e1426f7b97faf694ad18b4e069b7718bcc0
vielooo <arordavid@gmail.com>
2021-06-30 13:52:50 +0800

    lab1 don3

--- lab1/ex2.asm
+++ lab1/ex2.asm
@@ -1,4 +1,4 @@
-lui s0,0xFFFFF
+lui s0,0xFFFFF      # addr const 
 addi a0,x0,0x20     # save restart flag const 'b0010000
 addi a1,x0,0x40     # const 'b01000000
 addi a2,x0,0x80     #const 'b10000000
@@ -13,45 +13,49 @@ lw s2,0x72(s0)
 andi s3,s2,0x20     # get sw21
 beq s3,a0,restart # if sw21 = 1 , restart
 andi s3,s2,0x40     # get sw22
-beq s3,a1,xmx # if sw22 =1 ,go x*x 
+beq s3,a1,xmx   # if sw22 =1 ,go x*x 
 andi s3,s2,0x80 # get sw23 
-beq s3,a2,xmxmx # if sw23 =1 x*x 
+beq s3,a2,xmxmx # if sw23 =1 go x*x*x 
 jal restart     # if sw21,22,23 =0, restart 
-#------- x*x -------#
+#------- x*x -------
 xmx:
 add s4,x0,x0    #counter 
-add s5,x0,s1 # use s5 as mul
-slli s6,s1,4 # s6 is basic, answer in s7
-add s7,x0,x0
+or s5,x0,s1 # use s5 as mul
+slli s6,s1,4 # s6 = s1 << 4
+add s7,x0,x0    #final anwser saved in s7
 loop1:
-bge s4,a4,end#if counter >= 4 goto end 
+bge s4,a4,end   #if counter >= 4 goto end 
 andi s8,s5,1    # get the end of the mul 
 bne s8,a3,notmul1     # if the end of the mul = 1 
 add s7,s7,s6       # add basic s6
 notmul1:
 srli s7,s7,1    #s7 >>1
 srli s5,s5,1    #s5 >>1 
+addi s4,s4,1    #counter ++
 jal loop1
 #---------x*x*x-----------
 xmxmx:
+#----------firstly, get x*x in s7 
 add s4,x0,x0    #counter 
 add s5,x0,s1 # use s5 as mul
 slli s6,s1,4 # s6 is basic, answer in s7
 add s7,x0,x0
 loop2:
 bge s4,a4,next  # finish cal x*x goto next to cal x*x*x
-andi s8,s5,1
+andi s8,s5,1   
 bne s8,a3,notMul2
 add s7,s7,s6
 notMul2:
 srli s7,s7,1
 srli s5,s5,1
+addi s4,s4,1
 jal loop2
+#---------secondly, use s7 to get x*x*x
 next:   # x*x is in s7 
 add s4,x0,x0    #counter 
 add s5,x0,s1 # use s5 as mul
 slli s6,s7,4 # s6 is basic, answer in s7
-slli s7,s7,4
+add s7,x0,x0
 loop3:
 bge s4,a4,end
 andi s8,s5,1
@@ -60,20 +64,21 @@ add s7,s7,s6
 notMul3:
 srli s7,s7,1
 srli s5,s5,1
+addi s4,s4,1
 jal loop3
-#---------------------
+#----------End-----------
 end:
 # send s7 to led 
 sw s7,0x60(s0)
 loopread:
-# read again 
+# read sw 21,21,23 again 
 lw s2,0x72(s0)
-andi s3,s2,0x20
-beq s3,a0,reset     # if sw21 =1 , 
+andi s3,s2,0x20     # get sw21
+beq s3,a0,reset     # if sw21 =1 , reset the caculator
 jal loopread
 #----------
 reset:
-add s1,x0,x0
+add s1,x0,x0    #use x0 to reset all temp reg
 add s2,x0,x0
 add s3,x0,x0
 add s4,x0,x0
@@ -81,7 +86,7 @@ add s5,x0,x0
 add s6,x0,x0
 add s7,x0,x0
 add s8,x0,x0
-sw s0,0x60(s0)
+sw s1,0x60(s0)  #reset leds
 jal restart
 
 
--- lab1/ex3.asm
+++ lab1/ex3.asm
@@ -1,50 +1,110 @@
 ori a0,x0,100 # led1 loop times const 
 ori a1,x0,100 # lea2 loop times const 
-ori a2,x0,100 # led3 loop times const 
+ori a2,x0,150 # led3 loop times const 
 ori a3,x0,1     # const 1
-lui a3,0xFFFFF
+lui a4,0xFFFFF  #  address const 
+addi a5,x0,1    
+slli a5,a5,23   # const 0x800000
+add a6,x0,x0	#flag
+#--------const end ----------
+#-------- loop 1 ------------------
 loop1:
 #init led1
-add s0,x0,x0
-ori s3,x0,0x8000
-ori s4,x0,0x1
+add s0,x0,x0    #s0 is delay timer 
+ori s4,x0,1     # s4 control upper 12 led lights 
+slli s4,s4,11   
+ori s3,x0,0x1   # s3 control bottom 12 leds 
 led1:
-## send s3,s4 into led 
+# send s3,s4 signal to led 
 andi s5,s4,0xF
 slli s5,s5,12
 or s5,s5,s3
-sw s5,0x60(a3)
-srai s5,s4,4
-sw s5,0x60(a3)
-#flash method1
-andi s6,s3,1
-beq s6,a3,turnoff
+sw s5,0x60(a4)
+srli s5,s4,4
+sw s5,0x62(a4)
+## ----------control unit 1
+## if all lights off 
+beq s4,x0,loop2     # goto next flash method 
+## if all lights are turned on
+beq a6,a3,allon1
+andi s5,s4,0x1
+beq s5,a3,allon1
+## if there are some lights off, update s3,s4 
+slli s3,s3,1
+ori s3,s3,1
+srli s4,s4,1
+addi s8,x0,0x1
+slli s8,s8,11
+or s4,s4,s8
+jal delay1  ## goto delay 1 unit 
+allon1:## if all lights have been turned on, just shift s3,s4
+addi a6,x0,1
 srli s3,s3,1
-ori s3,s3,0x
-turnoff:
-
-
+slli s4,s4,1
 #--------finish update,start to delay 0.5s 
-
-
-
+delay1:
+bgt s0,a0,led1
 addi s0,s0,1
-jal led1
+jal delay1
 
+#------------loop2 -------------
 loop2:
+# initialize led2
 add s1,x0,x0
+ori,s3,x0,1
+slli s3,s3,23   #use s3 control all 24 leds 
 led2:
-bgt s1,a1,loop3
-#flash method2
+##----send s3 into led 
+or s4,s3,x0
+sw s4,0x60(a4)
+srli s4,s4,16
+sw s4,0x62(a4)
+## if all lights off 
+beq s3,x0,loop3
+## if all lights on 
+andi s5,s3,1
+beq s5,a3,allon2
+## not all on
+srli s3,s3,1
+or s3,s3,a5
+jal delay2
+allon2:
+srli s3,s3,1
+delay2:
+bgt s1,a1,led2
 addi s1,s1,1
-jal led2
+jal delay2
 
-
-
+#-----------loop3--------------
 loop3:
 add s2,x0,x0
+add s3,x0,x0
+add s4,x0,x0
+add s5,x0,x0
+lw s3,0x70(a4)	#count in s3
+## init led3 primitive lights 
+enlight:
+beq s5,s3,led3
+srli,s4,s4,1
+or s4,s4,a5
+addi s5,s5,1
+jal enlight
 led3:
-bgt s2,a2,loop1
-#flash method3
+## send s4 to reg 
+or s6,s4,x0
+sw s6,0x60(a4)
+srli s6,s6,16
+sw s6,0x62(a4)
+## if the end of the s4=1
+andi s5,s4,1
+beq s5,a3,loopshift
+## if end is zero 
+srli s4,s4,1
+jal delay3
+loopshift:
+srli s4,s4,1
+or s4,s4,a5
+delay3:
+bgt s2,a2,led3
 addi s2,s2,1
-jal led3
+jal delay3
